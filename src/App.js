6bbfb9a33e4b35b5d100fa8ddbd3bc56196c7610
Corey <cwills788@gmail.com>
2022-09-18 18:54:45 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+
 import './App.css';
 
 function App() {
+  const [firstVal, setFirstVal] = useState('')
+  const [secondVal, setSecondVal] = useState('')
+  const [thirdVal, setThirdVal] = useState('');
+  const [list, setList] = useState([]);
+
+  const clearInputs = () => {
+    setFirstVal('');
+    setSecondVal('');
+    setThirdVal('');
+  }
+
+  const handleSubmit = () => {
+    let newItem = `${firstVal}-${secondVal}-${thirdVal}`;
+    setList([...list, newItem])
+    clearInputs();
+  }
+
+  let listItems;
+  if(list.length > 1) {
+    listItems = list.map((item, idx) => {
+      return (
+        <h3 key={idx}>{item}</h3>
+      )
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input value={firstVal} onChange={(e) => setFirstVal(e.target.value)} />
+        <input value={secondVal} onChange={(e) => setSecondVal(e.target.value)} />
+        <input value={thirdVal} onChange={(e) => setThirdVal(e.target.value)} />
+        <button onClick={handleSubmit}>Add Drug</button>
+      </div>
+      {listItems}
     </div>
   );
 }
